f45e3aaa144ecec45ac8b02ab083308145aeb9f8
Sargis Khachatryan <sargis.khachatryan@picsart.com>
2021-06-06 20:47:35 +0400

    added global linking and compilation flags for bazel

--- container/test_array.cpp
+++ container/test_array.cpp
@@ -1,5 +1,12 @@
 #include <gtest/gtest.h>
+#include "array.hpp"
+
+using namespace skh;
 
 TEST(array_test, constructors) {
+    array<int, 2> a;
 
+    EXPECT_EQ(a[0], 0);
+    EXPECT_EQ(a.size(), 2);
+    
 }
